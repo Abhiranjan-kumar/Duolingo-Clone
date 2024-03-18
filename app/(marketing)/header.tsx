c340e841d248d89bd6d86ec6e0e8635ede9e7754
Abhiranjan-kumar <abhiranjan746@gmail.com>
2024-03-18 18:43:44 +0530

    Add Header

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { 
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton, 
+} from "@clerk/nextjs"
+import { Loader } from "lucide-react"
 import Image from "next/image"
 
 export const Header = () => {
@@ -6,7 +16,31 @@ export const Header = () => {
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
           <Image src="/mascot.svg" height={40} width={40} alt="Mascot"/>
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Duolingo
+          </h1>
         </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton 
+              afterSignOutUrl="/"
+            />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton 
+              mode="modal" 
+              afterSignInUrl="/learn"
+              afterSignUpUrl="/learn"
+            >
+              <Button size="lg" variant="ghost">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
       </div>
     </header>
   )
